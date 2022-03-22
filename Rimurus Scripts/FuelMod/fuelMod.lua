034b598a077dcb00da5984582a4e8eed38b86199
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-03-22 05:40:02 +0000

    improvements to fuelmod

--- Rimurus Scripts/FuelMod/fuelMod.lua
+++ Rimurus Scripts/FuelMod/fuelMod.lua
@@ -1,29 +1,37 @@
 require("fuelMod\\LuaUI")
 
+local maxFuelLevel = 100
+local fuelLevel = maxFuelLevel
+local fuelFill = 5
 
-local fuelLevel = 100
-local fuelDepreciation = 2
-function getFuelLevel(level)
-
+function fuelLevelDecreaseLevel()
     if player.is_player_in_any_vehicle(player.player_id()) then
-        
+           if utils.time_ms() + 900000 > utils.time_ms() then
+            if(fuelLevel >= 0 and entity.get_entity_speed(ped.get_vehicle_ped_is_using(player.get_player_ped(player.player_id()))) > 4) then
+                fuelLevel = fuelLevel - 0.003
+            end
+        end
     end
-    return level
 end
 
+local orange2 = {r=255, g=128, b=0, a=255}
 function drawFuelBar()
-    local orange = {r=243, g=156, b=18, a=255}
-    local orange2 = {r=255, g=128, b=0, a=255}
-    drawRect(0.1, 0.8, 0.13, 0.01, orange2) --background
-    drawRect(0.1, 0.8, fuelLevel/555, 0.01, orange) --fuel level 
+    fuelLevelDecreaseLevel()
+    if(ped.is_ped_in_any_vehicle(player.get_player_ped(player.player_id()))) then
+        LuaUI.drawRect(0.5, 0.98, fuelLevel/400, 0.02, orange2)
+        LuaUI.drawText(string.format("Fuel: %i/%i", math.floor(fuelLevel), maxFuelLevel), 0.5, 0.945, 0, 0.3, true, false)
+    end
 end
 
 function fuelMod()
     drawFuelBar()
+    if(fuelLevel <= 0) then
+        vehicle.set_vehicle_engine_on(player.get_player_vehicle(player.player_id()), false, true, true)
+    end    
 end
 
-menu.add_feature("Togggle fuelMod", "toggle", 0, function(tog)
-    while tog.on do
+menu.add_feature("Toggle FuelMod", "toggle", 0, function(tog)
+    while tog do
         fuelMod()
         system.wait(0)
     end
--- Rimurus Scripts/FuelMod/fuelMod/LuaUI.lua
+++ Rimurus Scripts/FuelMod/fuelMod/LuaUI.lua
@@ -1,37 +1,57 @@
 
+Colour = {
+	pastelGreen = {r=167, g=244, b=163, a=255},
+	white = {r=255, g=255, b=255, a=255},
+	black = {r=0, g=0, b=0, a=150},
+	blue = {r=137, g=196, b=244, a=255},
+	orange = {r=243, g=156, b=18, a=255}
+}
+
+LuaUI = { 
+	Options = { 
+		 scroll = 0,
+		 maxScroll = 0,
+		 menuWH = v2(),
+		 menuPos = v2(),
+		 currentMenu = "",
+		 menus = {}
+		}
+}
+
 --Shader Stuffs
-function drawRect(x, y, width, height, colour)
+function LuaUI.drawRect(x, y, width, height, colour)
 	ui.draw_rect(x, y, width, height, colour.r, colour.g, colour.b, colour.a)
 end
 
-function drawOutline(title, scale, x, y, w, h, colour, fillColour, filled)
+function LuaUI.drawOutline(title, scale, x, y, w, h, borderColour, fillColour, filled, spliiter)
 	filled = filled or false
+	spliiter = spliiter or false
 	title = title or ""
 
-	local white = {r=255, g=255, b=255, a=255}
-	
-	if(filled) then
-		drawRect(x, y+h/2, w, h, fillColour)
+	if (filled) then
+		LuaUI.drawRect(x, y+h/2, w, h, fillColour)
 	end
 	
-	ui.set_text_color(white.r, white.g, white.b, white.a)
+	ui.set_text_color(Colour.white.r, Colour.white.g, Colour.white.b, Colour.white.a)
 	ui.set_text_font(1)
 	ui.set_text_scale(scale, scale)
 	ui.set_text_outline(scale)
 	ui.set_text_centre(true)
-	ui.draw_text(title, v2(x, y))
+	ui.draw_text(tostring(title), v2(x, y))
 	
-	drawRect(x, y, w, 0.003, colour)
-	drawRect(x-w/2, y+h/2, 0.003, h,colour)
-	drawRect(x+w/2, y+h/2, 0.003, h, colour)
-	drawRect(x, y+h, w, 0.003, colour) 
+	LuaUI.drawRect(x, y, w, 0.003, borderColour)
+	LuaUI.drawRect(x-w/2, y+h/2, 0.003, h,borderColour)
+	LuaUI.drawRect(x+w/2, y+h/2, 0.003, h, borderColour)
+	LuaUI.drawRect(x, y+h, w, 0.003, borderColour) 
+
+	if spliiter then
+		LuaUI.drawRect(x, y + 0.026, w, 0.003, borderColour)
+	end
 end
 
 --Text Stuffs
-function drawText(text, x, y, font, scale, center, alignRight)
-	local white = {r=255, g=255, b=255, a=255}
-
-	ui.set_text_color(white.r, white.g, white.b, white.a)
+function LuaUI.drawText(text, x, y, font, scale, center, alignRight)
+	ui.set_text_color(Colour.white.r, Colour.white.g, Colour.white.b, Colour.white.a)
 	ui.set_text_font(font)
 	ui.set_text_scale(scale, scale)
 	ui.set_text_outline(1)
@@ -42,18 +62,35 @@ function drawText(text, x, y, font, scale, center, alignRight)
 			ui.set_text_right_justify(true)
 		end
 	
-	ui.draw_text(text, v2(x, y))
+	ui.draw_text(tostring(text), v2(x, y))
 end
 
 --Menu Functions
-function drawToggle(text, x, y, scale, bool)
-	local white = {r=255, g=255, b=255, a=255}
+function LuaUI.drawOption(text, num)
+    LuaUI.drawText(tostring(text), LuaUI.Options.menuPos.x, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10), 1, 0.5, true, false)
+end
 
-	if bool then
-	drawRect(x -0.05, y + 0.013, 0.01, 0.015, white)
-		--	drawOutline("", 0, x - 0.05, y, 0.010, 0.010, white, white, true)
-	else
-		drawOutline("", 0, x - 0.05, y, 0.010, 0.010, white, white, false)
-	end
-	drawText(text, x, y, 1, scale, true, false)
+function LuaUI.drawOptionToggle(text, num, toggle)
+	LuaUI.drawText(tostring(toggle and 'on' or "off"), LuaUI.Options.menuPos.x + 0.06, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10) + 0.005, 0, 0.3, true, false)
+	LuaUI.drawText(tostring(text), LuaUI.Options.menuPos.x, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10), 1, 0.5, true, false)
+end
+
+function LuaUI.drawSubmenu(text, num)
+	LuaUI.drawText(tostring(text), LuaUI.Options.menuPos.x, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10), 1, 0.5, true, false)
+	LuaUI.drawText(">>", LuaUI.Options.menuPos.x + 0.05, LuaUI.Options.menuPos.y + 0.032 + (0.34 * num/10) + 0.005, 1, 0.5, true, false)
+end
+
+function LuaUI.drawStringSlider(text, num, option, sliderValue)
+	LuaUI.drawText("<"..tostring(option[sliderValue + 1])..">", LuaUI.Options.menuPos.x + 0.06, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10) + 0.005, 0, 0.3, true, false)
+	LuaUI.drawText(tostring(text), LuaUI.Options.menuPos.x, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10), 1, 0.5, true, false)
+end
+
+function LuaUI.drawIntSlider(text, num, option)
+	LuaUI.drawText("<"..tonumber(option)..">", LuaUI.Options.menuPos.x + 0.06, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10) + 0.005, 0, 0.3, true, false)
+	LuaUI.drawText(tostring(text), LuaUI.Options.menuPos.x, LuaUI.Options.menuPos.y + 0.035 + (0.34 * num/10), 1, 0.5, true, false)
+end
+
+function LuaUI.switchToSub(sub)
+	LuaUI.Options.scroll = 0
+	LuaUI.Options.currentMenu = LuaUI.Options.menus[sub]
 end
