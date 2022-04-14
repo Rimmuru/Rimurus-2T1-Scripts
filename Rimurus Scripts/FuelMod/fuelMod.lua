f9f2fda846bb92f793ddef66a4b461caf3269bd1
Purple Wizard <31554073+GoldenLys@users.noreply.github.com>
2022-04-14 22:22:26 +0200

    purple's touch

--- Rimurus Scripts/FuelMod/fuelMod.lua
+++ Rimurus Scripts/FuelMod/fuelMod.lua
@@ -1,16 +1,38 @@
 require("fuelMod\\LuaUI")
 require("fuelMod\\stations")
 
+local usedVehicles = {}
+
 local fuelStates = {
     maxFuelLevel = 100,
-    fuelLevel = 6,
+    fuelLevel = math.random(10, 35),
     fuelFill = 0.5,
-    depreciation = 0.001,
     canFuel = false
 }
 
-
-local usedVehicles = {}
+local fuelDepreciations = {}
+    fuelDepreciations[0] = 0.25   --Compacts  
+    fuelDepreciations[1] = 0.85   --Sedans  
+    fuelDepreciations[2] = 1.0    --SUVs  
+    fuelDepreciations[3] = 0.35   --Coupes  
+    fuelDepreciations[4] = 1.0    --Muscle  
+    fuelDepreciations[5] = 1.15   --Sports Classics  
+    fuelDepreciations[6] = 1.25   --Sports  
+    fuelDepreciations[7] = 1.5    --Super  
+    fuelDepreciations[8] = 0.75   --Motorcycles  
+    fuelDepreciations[9] = 0.5    --Off-road  
+    fuelDepreciations[10] = 0.5   --Industrial  
+    fuelDepreciations[11] = 0.35  --Utility  
+    fuelDepreciations[12] = 0.35  --Vans  
+    fuelDepreciations[13] = 0     --Cycles  
+    fuelDepreciations[14] = 0     --Boats  
+    fuelDepreciations[15] = 0     --Helicopters  
+    fuelDepreciations[16] = 0     --Planes  
+    fuelDepreciations[17] = 0.45  --Service  
+    fuelDepreciations[18] = 0.45  --Emergency  
+    fuelDepreciations[19] = 0.65  --Military  
+    fuelDepreciations[20] = 0.75  --Commercial
+    fuelDepreciations[21] = 0     --Trains 
 
 local function getMyCurrentVehicle()
     return ped.get_vehicle_ped_is_using(player.get_player_ped(player.player_id()))
@@ -32,7 +54,7 @@ end
 local function fuelLevelDecreaseLevel()
     if utils.time_ms() + 900000 > utils.time_ms() then
         if(fuelStates.fuelLevel > 0 and entity.get_entity_speed(getMyCurrentVehicle()) > 4) then
-            fuelStates.fuelLevel = fuelStates.fuelLevel - fuelStates.depreciation
+            fuelStates.fuelLevel = fuelStates.fuelLevel - (fuelDepreciations[vehicle.get_vehicle_class(getMyCurrentVehicle())] / 1000)
           
             if fuelStates.fuelLevel < 0 then --should fix -1 fuelLevel
                 fuelStates.fuelLevel = 0
@@ -46,8 +68,17 @@ local function checkIfCanRefuel()
     or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.GroveStreet) <= 2.4
     or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.SandyShores) <= 2.4
     or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Paleto) <= 2.4
-    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Strawberry) <= 2.4 then
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Chilliad) <= 2.4
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Strawberry) <= 2.4
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.LittleSeoul) <= 2.4
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.PopularStreet) <= 2.4
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Harmony) <= 2.4
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.MirrorPark) <= 2.4 
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Morningwood) <= 2.4
+    or Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), Stations.Vinewood) <= 2.4 then
         fuelStates.canFuel = true
+    else
+        fuelStates.canFuel = false
     end
 end
 
@@ -60,9 +91,12 @@ local function fuelLevelIncreaseLevel()
 end
 
 local function drawFuelBar()
-    if fuelStates.fuelLevel <= 10 then
+    if fuelStates.fuelLevel <= 15 then
         LuaUI.drawText(string.format("Low Fuel: %i/%i", math.floor(fuelStates.fuelLevel), fuelStates.maxFuelLevel), 0.5, 0.945, 0, 0.3, true, false)
         LuaUI.drawRect(0.5, 0.98, fuelStates.fuelLevel/400, 0.02, Colour.red)
+    elseif fuelStates.fuelLevel >= 85 then
+        LuaUI.drawText(string.format("Fuel: %i/%i", math.floor(fuelStates.fuelLevel), fuelStates.maxFuelLevel), 0.5, 0.945, 0, 0.3, true, false)
+        LuaUI.drawRect(0.5, 0.98, fuelStates.fuelLevel/400, 0.02, Colour.green)
     else
         LuaUI.drawRect(0.5, 0.98, fuelStates.fuelLevel/400, 0.02, Colour.orange2)
         LuaUI.drawText(string.format("Fuel: %i/%i", math.floor(fuelStates.fuelLevel), fuelStates.maxFuelLevel), 0.5, 0.945, 0, 0.3, true, false)
@@ -82,6 +116,12 @@ local function fuelMod()
         if fuelStates.fuelLevel == 0 then
             vehicle.set_vehicle_engine_on(getMyCurrentVehicle(), false, true, true)
         end 
+--   else
+--      if fuelStates.fuelLevel < 100 and ped.get_current_ped_weapon(player.get_player_ped(player.player_id())) == 883325847 
+--          and Get_Distance_Between_Coords(player.get_player_coords(player.player_id()), entity.get_entity_coords(usedVehicles.hash)) <= 2 
+--      then
+--          fuelStates.fuelLevel = fuelStates.fuelLevel + fuelStates.fuelFill
+--      end
     end
 end
 
--- Rimurus Scripts/FuelMod/fuelMod/stations.lua
+++ Rimurus Scripts/FuelMod/fuelMod/stations.lua
@@ -3,11 +3,18 @@ Stations = {
     Strawberry = v3(-720.801, -934.966, 18.017),
     Davis = v3(172.266, -1563.212, 27.623),
     SandyShores = v3(2007.918, 3772.873, 30.533),
-    Paleto = v3(174.811, 6601.134, 30.334)
+    Paleto = v3(174.811, 6601.134, 30.334),
+    Chilliad = v3(1702.809, 6418.350, 31.125),
+    LittleSeoul = v3(-527.469, -1208.325, 16.671),
+    PopularStreet = v3(821.748, -1028.274, 24.871),
+    Harmony = v3(263.967, 2608.679, 44.269),
+    MirrorPark = v3(1181.554, -332.793, 67.596),
+    Morningwood = v3(-1430.680, -280.017, 44.627),
+    Vinewood = v3(614.969, 264.504, 101.509)
 }
 
 local function drawMarker(pos)
-    graphics.draw_marker(1, pos, v3(0,0,0), v3(0,0,0), v3(2,2,2), 255, 25, 25, 200, false, true, 2, false, nil, "MarkerTypeVerticalCylinder", false)       
+    graphics.draw_marker(1, pos, v3(0,0,0), v3(0,0,0), v3(2,2,2), 240, 200, 80, 200, false, true, 2, false, nil, "MarkerTypeVerticalCylinder", false)       
 end
 
 local function drawFuelBlips()
@@ -16,6 +23,13 @@ local function drawFuelBlips()
     ui.add_blip_for_coord(Stations.Strawberry)
     ui.add_blip_for_coord(Stations.Davis)
     ui.add_blip_for_coord(Stations.Paleto)
+    ui.add_blip_for_coord(Stations.Chilliad)
+    ui.add_blip_for_coord(Stations.LittleSeoul)
+    ui.add_blip_for_coord(Stations.PopularStreet)
+    ui.add_blip_for_coord(Stations.Harmony)
+    ui.add_blip_for_coord(Stations.MirrorPark)
+    ui.add_blip_for_coord(Stations.Morningwood)
+    ui.add_blip_for_coord(Stations.Vinewood)
 end
 drawFuelBlips()
 
@@ -25,4 +39,11 @@ function drawFuelMarkers()
     drawMarker(Stations.Strawberry)
     drawMarker(Stations.Davis)
     drawMarker(Stations.Paleto)
+    drawMarker(Stations.Chilliad)
+    drawMarker(Stations.LittleSeoul)
+    drawMarker(Stations.PopularStreet)
+    drawMarker(Stations.Harmony)
+    drawMarker(Stations.MirrorPark)
+    drawMarker(Stations.Morningwood)
+    drawMarker(Stations.Vinewood)
 end
