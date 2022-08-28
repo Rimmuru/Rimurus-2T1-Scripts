f1e782f4492b48fb2fe247367082fe0ab1e67cc1
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-08-28 16:10:40 +0100

    Update moduleList.lua

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,5 +1,5 @@
 local moduleList = {
-    {name = "example", link = "https://github.com/Rimmuru/Rimurus-2T1-Scripts/blob/main/Rimurus%20Scripts/Modules/example.lua"},
+    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"},
     {name = "Rimurus Module", link = "link"},
     {name = "Deccos Module", link = "link1"},
     {name = "Kittys Module", link = "link2"}
