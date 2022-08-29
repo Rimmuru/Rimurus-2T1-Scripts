b61a43311970a95dbaa4b8e28cc173447bab7b1a
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-08-29 01:44:01 +0100

    remove non used modules

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,7 +1,4 @@
 local moduleList = {
-    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"},
-    {name = "Rimurus Module", link = "link"},
-    {name = "Deccos Module", link = "link1"},
-    {name = "Kittys Module", link = "link2"}
+    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"}
 }
 return moduleList
