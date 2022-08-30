46a2c2666483b5023701774dd02e4b35467a436b
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-08-30 15:34:53 +0100

    Update example.lua

--- Rimurus Scripts/Modules/example.lua
+++ Rimurus Scripts/Modules/example.lua
@@ -1,5 +1,9 @@
-local testParent = func.add_feature("example module", "parent", 0)
+local testParent = func.add_feature("Rimurus Hospital Module", "parent", 0)
 
-func.add_feature("example module", "parent", testParent.id, function()
-    func.notification("test")
+func.add_feature("Disable Health Regen", "toggle", testParent.id, function(f)
+    if f.on then
+        native.call(0x5DB660B38DD98A31, player.player_id(), 0.0)
+    else
+        native.call(0x5DB660B38DD98A31, player.player_id(), 1.0)
+    end
 end)
