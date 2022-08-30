28e4ce8041538fdc39bc04b124ea8a6c1f4ea482
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-08-30 15:08:20 +0100

    Update moduleList.lua

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,4 +1,5 @@
 local moduleList = {
-    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"}
+    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"},
+    {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua"}
 }
 return moduleList
