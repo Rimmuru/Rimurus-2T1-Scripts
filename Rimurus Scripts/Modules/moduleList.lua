7f0f6ab0558dc622bf234e2fc1ee307959291fdd
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-08-30 15:47:03 +0100

    fixed syntax

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,6 +1,6 @@
 local moduleList = {
     {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"},
-    {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua"}
+    {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua"},
     {name = "Femboy Lua", link = "https://raw.githubusercontent.com/Decuwu/femboylua/main/FemboyRimuruMenu.lua"}
 }
 return moduleList
