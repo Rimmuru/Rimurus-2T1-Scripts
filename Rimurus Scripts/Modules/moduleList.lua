2e23d7e6d4c6bef0cc117dd97804d6c9d3e900c0
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-09-14 19:39:44 +0100

    added author

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,7 +1,7 @@
 local moduleList = {
-    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua"},
-    {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua"},
-    {name = "Femboy_Lua", link = "https://raw.githubusercontent.com/Decuwu/femboylua/main/Femboy.lua"},
-    {name = "Vehicle Colour Changer", link = "https://raw.githubusercontent.com/racistnoob/2take1/main/colourchanger/main.lua"}
+    {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua", author = "Caru#1766"},
+    {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua", author = "GhostOne#0991"},
+    {name = "Femboy_Lua", link = "https://raw.githubusercontent.com/Decuwu/femboylua/main/Femboy.lua", author = "decuwu#1337"},
+    {name = "Vehicle Colour Changer", link = "https://raw.githubusercontent.com/racistnoob/2take1/main/colourchanger/main.lua", author = "noob#9000"}
 }
 return moduleList
