128001b35420c2f532d35565144aa92a1d6da09a
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-09-19 23:50:58 +0100

    security improvement

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,13 +1,13 @@
 local moduleList = {
+    {name = "Vehicle Colour Changer", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Vehicle%20Colour%20Changer.lua", author = "noob#9000"},
+    {name = "Copy IP", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Copy%20IP.lua", author = "unknown"},
     {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua", author = "Caru#1766"},
     {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua", author = "GhostOne#0991"},
-    {name = "Femboy_Lua", link = "https://raw.githubusercontent.com/Decuwu/femboylua/main/Femboy.lua", author = "decuwu#1337"},
-    {name = "Vehicle Colour Changer", link = "https://raw.githubusercontent.com/racistnoob/2take1/main/colourchanger/main.lua", author = "noob#9000"},
-    {name = "Crusty teleports", link = "https://raw.githubusercontent.com/0x2XPx/2Take1-script-Archive/updated-scripts/CrustyTeleporter.lua", author = "unknown"},
-    {name = "Copy IP", link = "https://raw.githubusercontent.com/0x2XPx/2Take1-script-Archive/updated-scripts/CopyIP.lua", author = "unknown"},
-    {name = "Shitty crash methods", link = "https://raw.githubusercontent.com/0x2XPx/2Take1-script-Archive/updated-scripts/shittycrashmethods.lua", author = "unknown"},
-    {name = "Rain Spawner", link = "https://raw.githubusercontent.com/0x2XPx/2Take1-script-Archive/old-scripts/RainSpawner_v0.2.lua", author = "GhostOne#0991"},
-    {name = "Escort", link = "https://raw.githubusercontent.com/0x2XPx/2Take1-script-Archive/old-scripts/escort.lua", author = "INI2Moist"},
-    {name = "RC Troll", link = "https://raw.githubusercontent.com/0x2XPx/2Take1-script-Archive/old-scripts/RC_Trolls.lua", author = "unknown"}
+    {name = "Femboy_Lua", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Femboy_Lua.lua", author = "decuwu#1337"},
+    {name = "Crusty teleports", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Crusty%20teleports.lua", author = "unknown"},
+    {name = "Shitty crash methods", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Shitty%20crash%20methods.lua", author = "unknown"},
+    {name = "Rain Spawner", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Rain%20Spawner.lua", author = "GhostOne#0991"},
+    {name = "Escort", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Escort.lua", author = "INI2Moist"},
+    {name = "RC Troll", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/RC%20Troll.lua", author = "unknown"}
 }
 return moduleList
