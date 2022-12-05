6f8e6e6d2de437b9575b50af7ac00ca6f7015fd0
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-12-05 16:50:51 +0000

    tmp: remove colour changer

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,5 +1,4 @@
 local moduleList = {
-    {name = "Vehicle Colour Changer", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Vehicle%20Colour%20Changer.lua", author = "noob#9000"},
     {name = "Copy IP", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Copy%20IP.lua", author = "unknown"},
     {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua", author = "Caru#1766"},
     {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua", author = "GhostOne#0991"},
