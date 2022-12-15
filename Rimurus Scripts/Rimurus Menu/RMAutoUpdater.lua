670713b19d681776a54c64a56c28915f305eacce
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-12-15 00:51:54 +0000

    updated again

--- Rimurus Scripts/Rimurus Menu/RMAutoUpdater.lua
+++ Rimurus Scripts/Rimurus Menu/RMAutoUpdater.lua
@@ -1,15 +1,15 @@
 local files = {
     {name = "RimurusMenu", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/RimurusMenu.luac"},
-    {name = "GLTW", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/GLTW.lua"},
-    {name = "Get Input", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/Get%20Input.luac"},
-    {name = "Functionslib", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/Functionslib.luac"},
-    {name = "moduleInstaller", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/moduleInstaller.luac"},
-    {name = "settings", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/settings.luac"},
-    {name = "playerOptions", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/playerOptions.luac"},
-    {name = "Aimbot", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/Aimbot.luac"},
-    {name = "Drone", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/Drone.luac"},
-    {name = "World", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/World.luac"},
-    {name = "editor", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/editor.luac"}
+    {name = "Rimmuru\\libs\\GLTW", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/GLTW.lua"},
+    {name = "Rimmuru\\libs\\Get Input", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/Get%20Input.luac"},
+    {name = "Rimmuru\\libs\\Functionslib", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/Functionslib.luac"},
+    {name = "Rimmuru\\libs\\moduleInstaller", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/moduleInstaller.luac"},
+    {name = "Rimmuru\\libs\\settings", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/settings.luac"},
+    {name = "Rimmuru\\libs\\feats\\playerOptions", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/playerOptions.luac"},
+    {name = "Rimmuru\\libs\\feats\\Aimbot", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/Aimbot.luac"},
+    {name = "Rimmuru\\libs\\feats\\Drone", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/Drone.luac"},
+    {name = "Rimmuru\\libs\\feats\\World", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/World.luac"},
+    {name = "Rimmuru\\libs\\feats\\editor", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Rimurus%20Menu/Rimuru/libs/feats/editor.luac"}
 }
 
 return files
