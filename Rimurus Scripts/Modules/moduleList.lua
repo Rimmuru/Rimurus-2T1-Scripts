8fc15ec4dbf2db479e9e67dd5be9e6483a3749ae
Decuwu <112495774+Decuwu@users.noreply.github.com>
2023-01-07 00:38:24 +0000

    Update moduleList.lua

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,5 +1,6 @@
 local moduleList = {
     {name = "Femboy Lua", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Femboy_Lua.lua", author = "Decuwu"},
+    {name = "Tophs Overlay", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/TophsOverlay.lua", author = "Toph"},
     {name = "Copy IP", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Copy%20IP.lua", author = "unknown"},
     {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua", author = "Caru#1766"},
     {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua", author = "GhostOne#0991"},
@@ -8,7 +9,6 @@ local moduleList = {
     {name = "Rain Spawner", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Rain%20Spawner.lua", author = "GhostOne#0991"},
     {name = "Escort", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Escort.lua", author = "INI2Moist"},
     {name = "RC Troll", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/RC%20Troll.lua", author = "unknown"},
-    {name = "ScriptHostFuckery", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/ScriptHostFuckery.lua", author = "DemonKiya(updated by Rimuru)"},
-    {name = "Tophs Overlay", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/TophsOverlay.lua", author = "Toph"}
+    {name = "ScriptHostFuckery", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/ScriptHostFuckery.lua", author = "DemonKiya(updated by Rimuru)"}
 }
 return moduleList
