9ceafe68ee1f1e151ac3c0ed42bfb39f3c842f3e
Decuwu <112495774+Decuwu@users.noreply.github.com>
2023-01-07 00:24:37 +0000

    a

--- Rimurus Scripts/Modules/moduleList.lua
+++ Rimurus Scripts/Modules/moduleList.lua
@@ -1,5 +1,4 @@
 local moduleList = {
-    {name = "Femboy Luac", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Femboy.luac", author = "Decuwu"}
     {name = "Copy IP", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Copy%20IP.lua", author = "unknown"},
     {name = "example", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/example.lua", author = "Caru#1766"},
     {name = "Color_changer_with_preview", link = "https://raw.githubusercontent.com/Rimmuru/Rimurus-2T1-Scripts/main/Rimurus%20Scripts/Modules/Color_changer_with_preview.lua", author = "GhostOne#0991"},
